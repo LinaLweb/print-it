bdbc83e1a433aa6da2ad5939d853e065e399af15
Lina Longeau <lina.longeau@gmail.com>
2023-12-13 16:12:12 +0100

    modification des chemins pour les images dans le tableau et ajout de la function updateSlider + du calcul dans const arrowLeft et const arrowRight pour que les images reculent  quand on clique sur la flèche de gauche et que les images avancent quand on clique sur la flèche de droite

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,47 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 let index = 0;
 
-const changingDots = document.querySelectorAll(".dot")
+const changingDots = document.querySelectorAll(".dot");
 
-const changingSlides = document.querySelector(".banner-img")
+const changingSlides = document.querySelector(".banner-img");
 
 const arrowLeft = document.querySelector(".arrow_left").addEventListener("click",  () => {
+	index = (index -1 + slides.length) % slides.length;
+	updateSlider();
 	console.log("Vous avez cliqué sur la flèche gauche")
- })
+ });
 
 const arrowRight = document.querySelector(".arrow_right").addEventListener("click", () => {
+	index = (index + 1) % slides.length;
+	updateSlider();
 	console.log("Vous avez cliqué sur la flèche droite")
- })
+ });
+
+ function updateSlider() {
+	const currentSlide = slides[index];
+	changingSlides.src = currentSlide.image;
+	changingSlides.alt = currentSlide.tagLine;
+ }
+
+ //  mettre à jour les points//
+
+
 
