7e11872896f597ae64ca0e1a68c0d5c5ece4c74a
Lina Longeau <lina.longeau@gmail.com>
2023-12-13 18:15:50 +0100

    ajout de la boucle pour la mise à jour des bullets points sur les images dans le carousel dans la fuction updateSlider + ajout de la classe pour le bullet point actuel + aujout  de la classe pour faire la supression de la classe des autres bullets points

--- assets/script.js
+++ assets/script.js
@@ -24,24 +24,30 @@ const pointsDots = document.querySelectorAll(".dot");
 const imagesSlides = document.querySelector(".banner-img");
 
 const arrowLeft = document.querySelector(".arrow_left").addEventListener("click",  () => {
-	index = (index -1 + slides.length) % slides.length;
+	index = (index -1 + slides.length) % slides.length;                                      //calcul pour que les images reculent quand on clique sur la flèche gauche//
 	updateSlider();
 	console.log("Vous avez cliqué sur la flèche gauche")
  });
 
-const arrowRight = document.querySelector(".arrow_right").addEventListener("click", () => {
-	index = (index + 1) % slides.length;
+const arrowRight = document.querySelector(".arrow_right").addEventListener("click", () => {   
+	index = (index + 1) % slides.length;                                                     //calcul pour que les images avancent quand on clique sur la flèche droite//
 	updateSlider();
 	console.log("Vous avez cliqué sur la flèche droite")
  });
 
- function updateSlider() {
+ function updateSlider() {                //fonction pour la mise à jour des images dans le carousel//
 	const currentSlide = slides[index];
 	imagesSlides.src = currentSlide.image;
 	imagesSlides.alt = currentSlide.tagLine;
+
+	pointsDots.forEach(dot => {              // boucle pour la mise à jour des bullets points sur les images dans le carousel//
+		dot.classList.add("dot_selected");  // ajoute la classe pour le bullet point actuel//
+	});
+	pointsDots[index].classList.remove("dot_selected"); //supprime la classe pour les autres bullets points//
  }
 
- //  mettre à jour les points//
+
+ //reste à faire pour le tagline les phrases qui se mettent à jour en même temps que les images dans le carousel//
 
 
 
